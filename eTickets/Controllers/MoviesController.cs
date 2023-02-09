ca137456ded66235766d4b498b4a90490c816efa
hrechaniuk.bohdan <bodjeywh29@gmail.com>
2023-02-09 17:01:27 +0200

    +1 to 14-22

--- eTickets/Controllers/MoviesController.cs
+++ eTickets/Controllers/MoviesController.cs
@@ -1,11 +1,19 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using eTickets.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace eTickets.Controllers
 {
     public class MoviesController : Controller
     {
-        public IActionResult Index()
+        private readonly AppDbContext _context;
+        public MoviesController(AppDbContext context)
         {
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var data = await _context.Movies.ToListAsync();
             return View();
         }
     }
