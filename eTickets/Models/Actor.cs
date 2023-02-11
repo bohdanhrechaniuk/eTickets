5a2c85e745eb221311a33320fb49f31fef99f256
bohdan.hrechaniuk <bodjeywh29@gmail.com>
2023-02-11 23:08:39 +0200

    Implemented services,added more views, implemented Create and Details ...

--- eTickets/Models/Actor.cs
+++ eTickets/Models/Actor.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System.ComponentModel.DataAnnotations;
 
 namespace eTickets.Models
 {
@@ -7,12 +8,19 @@ namespace eTickets.Models
         [Key]
         public int Id { get; set; }
         [Display(Name ="Profile Picture")]
+        [Required(ErrorMessage ="Profile picture is required")]
         public string ProfilePictureURL { get; set; }
+
         [Display(Name = "Full Name")]
+        [Required(ErrorMessage = "Full Name is required")]
+        [StringLength(50,MinimumLength =3,ErrorMessage ="Full Name must be between {2} and {1} chars")]
         public string FullName { get; set; }
         [Display(Name = "Biography")]
+        [Required(ErrorMessage = "Biography is required")]
         public string Bio { get; set; }
 
+        //Relationships
+        [ValidateNever]
         public List<Actor_Movie> Actor_Movies { get; set; }
     }
 }
