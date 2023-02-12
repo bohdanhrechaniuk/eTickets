e068c76596b79e87725b02a5656957075397fa80
bohdan.hrechaniuk <bodjeywh29@gmail.com>
2023-02-12 14:48:35 +0200

    Additional to previous commit.

--- eTickets/Controllers/ProducersController.cs
+++ eTickets/Controllers/ProducersController.cs
@@ -17,5 +17,12 @@ namespace eTickets.Controllers
             var allProducers = await _service.GetAllAsync();
             return View(allProducers);
         }
+        //GET: producers/details/1
+        public async Task<IActionResult> Details(int id)
+        {
+            var producerDetails = await _service.GetByIdAsync(id);
+            if (producerDetails == null) return View("NotFound");
+            return View(producerDetails);
+        }
     }
 }
