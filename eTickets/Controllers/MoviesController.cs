a4a3574307a36b98ab4614c053ed1cbce9c54a2b
bohdan.hrechaniuk <bodjeywh29@gmail.com>
2023-02-14 13:32:11 +0200

    Added : Create View ,new movies to db,dropdowns,_CreateItem partial view

--- eTickets/Controllers/MoviesController.cs
+++ eTickets/Controllers/MoviesController.cs
@@ -1,6 +1,7 @@
 ﻿using eTickets.Data;
 using eTickets.Data.Services;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 
 namespace eTickets.Controllers
@@ -21,6 +22,17 @@ namespace eTickets.Controllers
         {
             var movieDetail = await _service.GetMovieByIdAsync(id);
             return View(movieDetail);
+        }
+        //GET: Movies/Create
+        public async Task<IActionResult> Create()
+        {
+            var movieDropdownsData = await _service.GetNewMovieDropdownsValues();
+
+            ViewBag.Cinemas = new SelectList(movieDropdownsData.Cinemas, "Id", "Name");
+            ViewBag.Producers = new SelectList(movieDropdownsData.Producers, "Id", "FullName");
+            ViewBag.Actors = new SelectList(movieDropdownsData.Actors, "Id", "FullName");
+
+            return View();
         }
     }
 }
